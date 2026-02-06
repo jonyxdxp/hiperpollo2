7a817820e7f1ea3cea5e2eb18e965504a47a60cb
jonyxdxp <jonyxdxp@gmail.com>
2026-02-06 11:31:40 -0300

    dfsb

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
-  },
   plugins: [
     react(),
     VitePWA({
